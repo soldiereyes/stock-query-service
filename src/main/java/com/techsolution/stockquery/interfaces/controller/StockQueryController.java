77513bc14af08ad887a116488465547a71deafa1
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2025-12-19 19:50:29 -0300

    feat: limpeza e identação

--- src/main/java/com/techsolution/stockquery/interfaces/controller/StockQueryController.java
+++ src/main/java/com/techsolution/stockquery/interfaces/controller/StockQueryController.java
@@ -5,7 +5,6 @@ import com.techsolution.stockquery.application.service.StockQueryService;
 import com.techsolution.stockquery.domain.model.StockView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/test/java/com/techsolution/stockquery/interfaces/controller/StockQueryControllerTest.java
+++ src/test/java/com/techsolution/stockquery/interfaces/controller/StockQueryControllerTest.java
@@ -14,8 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
